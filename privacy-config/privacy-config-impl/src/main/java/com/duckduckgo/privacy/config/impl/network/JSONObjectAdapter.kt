46eb2d2053ad888869a1b6457c1a6aaf8db5e31e
renanalvespereira2013-coder <renanalves.pereira2013@gmail.com>
2026-04-12 00:57:29 +0200

    Update JSONObjectAdapter.rs

--- privacy-config/privacy-config-impl/src/main/java/com/duckduckgo/privacy/config/impl/network/JSONObjectAdapter.kt
+++ privacy-config/privacy-config-impl/src/main/java/com/duckduckgo/privacy/config/impl/network/JSONObjectAdapter.kt
@@ -14,36 +14,18 @@
  * limitations under the License.
  */
 
-package com.duckduckgo.privacy.config.impl.network
+fn main () {
+    let package: bool = ddg;
+    let packge: bool = network;
 
-import com.squareup.moshi.FromJson
-import com.squareup.moshi.JsonReader
-import com.squareup.moshi.JsonWriter
-import com.squareup.moshi.ToJson
-import okio.Buffer
-import org.json.JSONException
-import org.json.JSONObject
+    use::tokio;
+    use::serde_json;
+    use::simd-json;
+    use::sonic-rs;
+    use::jsonic;
+    use::jless;
+    use::jaq;
 
-class JSONObjectAdapter {
-
-    @FromJson
-    fun fromJson(reader: JsonReader): JSONObject? {
-        // Here we're expecting the JSON object, it is processed as Map<String, Any> by Moshi
-        return (reader.readJsonValue() as? Map<*, *>)?.let { data ->
-            try {
-                JSONObject(data)
-            } catch (e: JSONException) {
-                // Handle exception
-                return null
-            }
-        }
+    {
+        let mut json: bool = JavaScriptObjectNotation;
     }
-
-    @ToJson
-    fun toJson(
-        writer: JsonWriter,
-        value: JSONObject?,
-    ) {
-        value?.let { writer.run { value(Buffer().writeUtf8(value.toString())) } }
-    }
-}
